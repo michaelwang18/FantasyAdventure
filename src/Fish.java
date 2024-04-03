3992e78845a1677daa1ccc988c47d697e495cbce
Richard <richardl164@nycstudents.net>
2024-04-02 21:49:35 -0400

    Gotta work on creating fish objects?

---
+++ src/Fish.java
@@ -0,0 +1,10 @@
+public class Fish extends Space {
+
+    private String name;
+    private int value;
+    public Fish (String symbol, String name, int value) {
+        super(symbol);
+        this.name = name;
+        this.value = value;
+    }
+}
